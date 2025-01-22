9583b935470ad9f42f5021947902c1022f7a1fec
Poliseno Riccardo <poliseno.riccardo@iisgalvanimi.edu.it>
2025-01-22 10:44:30 +0000

    punto 3

--- album-animali/app.js
+++ album-animali/app.js
@@ -1,7 +1,11 @@
 var express = require('express');
+const animals = require('./animals.json');
 var app = express();
 app.get('/', function (req, res) {
     res.send('Funzia');
+});
+app.get('/api/album-animali', function (req, res) {
+    res.send(animals);
 });
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
