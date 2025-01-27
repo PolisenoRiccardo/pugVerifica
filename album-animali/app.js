ea630d42a9fa86c931e5ed9494b463e0635711c4
Poliseno Riccardo <113579593+PolisenoRiccardo@users.noreply.github.com>
2025-01-27 08:47:05 +0000

    punto 4

--- album-animali/app.js
+++ album-animali/app.js
@@ -1,12 +1,29 @@
-var express = require('express');
+const express = require('express');
 const animals = require('./animals.json');
-var app = express();
-app.get('/', function (req, res) {
-    res.send('Funzia');
+
+const app = express();
+
+app.set('view engine', 'pug');
+
+app.get('/', (req, res) => {
+    res.render('index');
 });
 app.get('/api/album-animali', function (req, res) {
     res.send(animals);
 });
+app.get('/api/:id_figurina', (req, res) => {
+    const animalID = req.params.id_figurina;
+    const animal = animals.animal.find(s => s.id_figurina == animalID);
+
+    if (animal) {
+        res.render('animal', {
+            animal: animal
+        });
+    } else {
+        res.status(404).send('Animal not found');
+    }
+});
+
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
 });
