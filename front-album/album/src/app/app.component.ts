b49165af4f592fe6e1b782fe9a9d0a5cd50b3e53
Poliseno Riccardo <113579593+PolisenoRiccardo@users.noreply.github.com>
2025-01-28 07:12:03 +0000

    testing

--- front-album/album/src/app/app.component.ts
+++ front-album/album/src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http'; //HTTP CLIENT
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+selector: 'app-root',
+templateUrl: './app.component.html',
+styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'album';
+export class AppComponent implements OnInit{
+  results !: any;
+  // Inject HttpClient into your component or service.
+  constructor(private http: HttpClient) {}
+
+    ngOnInit(): void {
+      // Make the HTTP request:
+      this.http.get('http://localhost:3000/api/album-animali').subscribe(data => {
+      // Read the result field from the JSON response.
+      this.results = data;
+      console.log(this.results);
+      });
+    }
 }
